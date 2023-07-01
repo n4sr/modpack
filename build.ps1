f729bc686a52c68c7b0b8e9e264d97bc9a9a3d55
n4sr <n4sr@users.noreply.github.com>
2023-07-01 15:23:18 -0500

    added lithium and phosphor. fixed build script.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 $Parent_Path = Split-Path -Parent $PSCommandPath
+Remove-Item -Path "$Parent_Path\s.n4.cx.mrpack"
 Compress-Archive -Path $Parent_Path -DestinationPath "$Parent_Path\s.n4.cx.mrpack" -Force
