f0ee3a918982418132895d74fad84bafa7f452ce
n4 <54733427+n4sr@users.noreply.github.com>
2023-07-02 14:57:01 -0500

    added mods, updated build script.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 $Parent_Path = Split-Path -Parent $PSCommandPath
 Set-Location -Path "$Parent_Path\src"
 Compress-Archive -Path "$Parent_Path\src\*" -DestinationPath "$Parent_Path\s.n4.cx.mrpack" -Force
+Copy-Item -Path "$Parent_Path\s.n4.cx.mrpack" -Destination "$Parent_Path\s.n4.cx.zip" -Force
